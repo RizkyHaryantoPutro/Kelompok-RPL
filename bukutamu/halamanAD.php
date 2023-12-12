705636d831a2a44ee9c6a1896554562a9692b137
RizkyHaryantoPutro <147717931+RizkyHaryantoPutro@users.noreply.github.com>
2023-12-12 22:56:28 +0700

    Add files via upload

---
+++ bukutamu/halamanAD.php
@@ -0,0 +1,8 @@
+<?php include 'headerAD.php'; ?>
+<h3 align="center">SELAMAT DATANG DIHALAMAN PERPUSTAKAAN</h3>
+<br>
+<h3 align="center">UNIVERSITAS SEMARANG</h3>
+
+
+
+<?php include 'footer.php'; ?>
