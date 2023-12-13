22043d5b9cacd9474d59bc4b8800bc4abb4a798e
RizkyHaryantoPutro <147717931+RizkyHaryantoPutro@users.noreply.github.com>
2023-12-13 19:28:32 +0700

    Delete bukutamu directory

--- bukutamu/halamanAD.php
+++
@@ -1,8 +0,0 @@
-<?php include 'headerAD.php'; ?>
-<h3 align="center">SELAMAT DATANG DIHALAMAN PERPUSTAKAAN</h3>
-<br>
-<h3 align="center">UNIVERSITAS SEMARANG</h3>
-
-
-
-<?php include 'footer.php'; ?>
--- bukutamu/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$konek = mysqli_connect('localhost','root','','bukutamu');
-if (!$konek){
-	echo "can't find database named buku_tamu";
-}
-
- ?>
